296f5b28aa3f2f298a12e4ea0e3f499ad5092cbc
w-alke-r <purmwa@gmail.com>
2022-03-01 21:30:57 +0300

    tyu

---
+++ monthsdisplay.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class monthsdisplay 
+ {
+    public static void main(String[] args)
+    {
+        Scanner num= new Scanner(System.in);
+        System.out.println("Enter number of the month: ");
+          int k=num.nextInt();
+          if(k==1)
+          {
+              System.out.println("Month of Jan");
+          }
+          else if(k==2)
+          {
+              System.out.println("Month of February");
+          }
+          else if(k==3)
+          {
+              System.out.println("Month of March");
+          }
+          else if(k==4)
+          {
+              System.out.println("Month of April");
+          }
+          else if(k==5)
+          {
+              System.out.println("Month of May");
+          }
+          else if(k==6)
+          {
+              System.out.println("Month of June");
+          }
+          else if(k==7)
+          {
+              System.out.println("Month of July");
+          }
+          else if(k==8)
+          {
+              System.out.println("Month of August");
+          }
+          else if(k==9)
+          {
+              System.out.println("Month of September");
+          }
+          else if(k==10)
+          {
+              System.out.println("Month of October");
+          }
+          else if(k==11)
+          {
+              System.out.println("Month of November");
+          }
+          else if(k==12)
+          {
+              System.out.println("Month of December");
+          }
+   
+    }
+ }
